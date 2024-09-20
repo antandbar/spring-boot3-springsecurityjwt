9f1c1b9f937d0618b5665cc36bfa1a71f558592a
Antonio Andreu <antonio.andreu.barreiro@gmail.com>
2024-09-20 11:44:39 +0200

    feat: Clase para generar el excepciones en la autenticación

---
+++ src/main/java/com/antonio/curso/springsecurityjwt/springsecurityjwt/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,23 @@
+package com.antonio.curso.springsecurityjwt.springsecurityjwt.security;
+
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+//Clase para manejar las exceción de tipo autenticación en nuestra app
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+
+}
